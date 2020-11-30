dd2f4d142886503316e8090fe8f0f069fc639212
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2020-11-30 08:32:51 -0500

    remove extra type

--- src/type.ts
+++ src/type.ts
@@ -4,12 +4,6 @@ export interface StrawHatGlobals {
   STRAW_HAT_GLOBALS: {
     logger?: {
       level?: LogLevels;
-      metadata?: Record<any, any>;
-      handlers?: {
-        [name: string]: {
-          level?: LogLevels;
-        };
-      };
     };
   };
 }
